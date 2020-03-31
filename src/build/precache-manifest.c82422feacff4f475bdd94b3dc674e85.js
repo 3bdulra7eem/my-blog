a70a1b435fd22ca8e406b35b1fe5a5d557c4bd05
3bdulra7eem <master_man_jan@hotmail.com>
2020-03-31 15:37:22 +0300

    first commit

---
+++ src/build/precache-manifest.c82422feacff4f475bdd94b3dc674e85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48db36daa5b63a2a43b7fca60f1887a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a67a4ac9490da097b8b0",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "d4eb2f6700eddc29e824",
+    "url": "/static/js/2.0c6fa369.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0c6fa369.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a67a4ac9490da097b8b0",
+    "url": "/static/js/main.de0d4809.chunk.js"
+  },
+  {
+    "revision": "921ac687539ca31687de",
+    "url": "/static/js/runtime-main.7c2b1bab.js"
+  }
+]);
